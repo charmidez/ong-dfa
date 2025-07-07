1c263927f31c49b9eae3e027d85f06ee053e7960
charmidez <kodjoassiobo@protonmail.com>
2025-07-07 15:15:46 +0000

    SiteMap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ong-dfa.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
