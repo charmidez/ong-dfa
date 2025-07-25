130c1ba4d0460ecd9aadf3f96ee27f983ca7b3b8
Charmidez <charmidez.assiobo@gmail.com>
2025-07-25 10:26:32 +0000

    number changed

--- src/app/contact/contactSections.tsx
+++ src/app/contact/contactSections.tsx
@@ -13,8 +13,8 @@ export function ContactButton() {
     <div className="lg:py-16 py-8 px-4 lg:px-8">
       <TitreSection titre="Nous contacter" />
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-4 gap-2">
-        <Contactcard titre="Nous appeler directement ?" imgSrc="/icons/call.svg" linkToAction="tel:+22890871113" textButton="Par ici"/>
-        <Contactcard titre="Nous écrire par whatsapp ?" imgSrc="/icons/whatsapp.svg" linkToAction="https://wa.me/22890871113" textButton="Un sms par ici"/>
+        <Contactcard titre="Nous appeler directement ?" imgSrc="/icons/call.svg" linkToAction="tel:+22891270369" textButton="Par ici"/>
+        <Contactcard titre="Nous écrire par whatsapp ?" imgSrc="/icons/whatsapp.svg" linkToAction="https://wa.me/22891270369" textButton="Un sms par ici"/>
         <Contactcard titre="Nous écrire directement un mail ?" imgSrc="/icons/mail.png" linkToAction="mailto:contact@ong-dfa.com" textButton="Un mail par ici"/>
       </div>
     </div>
