da7c883d6e4700bd8ee083ec71148f548b9820ae
Charmidez Assiobo <charmidez.assiobo@gmail.com>
2025-09-11 15:37:15 +0000

    Addresse changé

--- src/data/globalData.tsx
+++ src/data/globalData.tsx
@@ -3,4 +3,4 @@ export const nomDuSiteDefini : string = "Development For All";
 
 export const nomDuSiteComplet : string = "DFA ONG - Development For All";
 
-export const addresseLocalisation : string = `ADIDOGOME YOKOE, 2ème Rue à droite après la station d'essence Energium à 300 mètre`
+export const addresseLocalisation : string = `ADIDOGOME YOKOE, 2ème Rue à droite après la station d'essence Energium à 300 mètres`
